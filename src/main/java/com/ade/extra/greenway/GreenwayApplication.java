de425e6cc4c804f93fdcdf6c67d4ed2fe383482e
liyang <ismeade99@sina.com>
2023-02-16 23:25:53 +0800

    feat:create

---
+++ src/main/java/com/ade/extra/greenway/GreenwayApplication.java
@@ -0,0 +1,13 @@
+package com.ade.extra.greenway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ade/extra/greenway/GreenwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ade.extra.greenway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
