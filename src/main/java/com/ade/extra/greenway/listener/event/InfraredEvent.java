e83846e37865068401fa300218f381873b04457f
Liyang <ismeade99@sina.com>
2023-03-03 15:02:36 +0800

    feat:事件定义

---
+++ src/main/java/com/ade/extra/greenway/listener/event/InfraredEvent.java
@@ -0,0 +1,22 @@
+package com.ade.extra.greenway.listener.event;
+
+/**
+ * 红外事件
+ */
+public class InfraredEvent {
+
+    /**
+     * 进入; 红外断开
+     */
+    public static class Off {
+
+    }
+
+    /**
+     * 未进入或离开; 红外接通
+     */
+    public static class On {
+
+    }
+
+}
---
+++ src/main/java/com/ade/extra/greenway/listener/event/StressEvent.java
@@ -0,0 +1,22 @@
+package com.ade.extra.greenway.listener.event;
+
+/**
+ * 地感线圈事件
+ */
+public class StressEvent {
+
+    /**
+     * 按压
+     */
+    public static class Press {
+
+    }
+
+    /**
+     * 抬起
+     */
+    public static class Loosen {
+
+    }
+
+}
